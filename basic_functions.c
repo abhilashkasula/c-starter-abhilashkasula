e1c5bc1554059d6a6cae149c220bcd1489a277de
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-28 17:06:23 +0530

    added calculateSI function to basic_functions.c

--- basic_functions.c
+++ basic_functions.c
@@ -37,9 +37,15 @@ float convertToFahrenheit(int temp) {
   return ((temp * 9.00) / 5) + 32;
 }
 
+float calculateSI(long int principle, int interest, int period) {
+  return (principle * interest * period) / 100.00;
+}
+
 int main(void) {
   int number = 2, num2 = 2, num3 = 3;
   int temp = 100;
+  int period = 2, interest = 8;
+  long int principle = 400;
   printf("%d is %s\n", number, is_even(number) ? "even" : "not even");
   printf("%d is %s\n", number, is_odd(number) ? "odd": "not odd");
   printf("Square of %d is %d\n", number, square(number));
@@ -48,4 +54,5 @@ int main(void) {
   printf("Greatest of %d, %d and %d is %d\n", number, num2, num3, greatestOfThree(number, num2, num3));
   printf("Centigrade of given Fahrenheit %d is %f\n", temp, convertToCentigrade(temp));
   printf("Fahrenheit of given Centigrade %d is %f\n", temp, convertToFahrenheit(temp));
+  printf("SI of Principle: %ld, Interest: %d, Period: %d is %f\n", principle, interest, period, calculateSI(principle, interest, period));
 }
