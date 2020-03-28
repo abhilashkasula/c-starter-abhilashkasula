c873ddba3ef3e04d7bfbe9fbd063ce40f6a4a287
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-28 16:25:15 +0530

    added greatesOfThree function to basic_functions.c

--- basic_functions.c
+++ basic_functions.c
@@ -21,11 +21,20 @@ float average(int num1, int num2, int num3) {
   return sum / 3.00;
 }
 
+int getGreatest(int num1, int num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+int greatestOfThree(int num1, int num2, int num3) {
+  return getGreatest(getGreatest(num1, num2), num3);
+}
+
 int main(void) {
-  int number = 5, num2 = 7, num3 = 7;
+  int number = 2, num2 = 2, num3 = 3;
   printf("%d is %s\n", number, is_even(number) ? "even" : "not even");
   printf("%d is %s\n", number, is_odd(number) ? "odd": "not odd");
   printf("Square of %d is %d\n", number, square(number));
   printf("Cube of %d is %d\n", number, cube(number));
-  printf("Average of %d, %d and %d is %f", number, num2, num3, average(number, num2, num3));
+  printf("Average of %d, %d and %d is %f\n", number, num2, num3, average(number, num2, num3));
+  printf("Greatest of %d, %d and %d is %d", number, num2, num3, greatestOfThree(number, num2, num3));
 }
