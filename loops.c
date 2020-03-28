b30a1a226ce1454331dbf51a3d79fcdb9a99d20d
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-28 19:51:21 +0530

    added fibonacci function to loops.c

--- loops.c
+++ loops.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 unsigned int factorial(int);
+int fibonacci(int);
+
+int fibonacci(int size) {
+  int first = 0, second = 1;
+  printf("Fibonacci Series:  ");
+  for(int index = 1; index <= size; index++) {
+    printf("%d, ", first);
+    int temp = second;
+    second += first;
+    first = temp;
+  }
+  return 0;
+}
 
 unsigned int factorial(int number) {
   unsigned long int fact = 1;
@@ -13,5 +26,6 @@ unsigned int factorial(int number) {
 int main(void) {
   int number = 5;
   printf("Factorial of %d is %u\n", number, factorial(number));
+  fibonacci(10);
   return 0;
 }
