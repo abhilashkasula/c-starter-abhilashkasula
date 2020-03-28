2ee8223e9a590cf3a741169f25c8aa8a7bdf337c
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-28 15:17:35 +0530

    added square and cube functions to basic_functions.c

--- basic_functions.c
+++ basic_functions.c
@@ -8,10 +8,20 @@ unsigned char is_odd(int number) {
   return !is_even(number);
 }
 
+int square(int number) {
+  return number * number;
+}
+
+int cube(int number) {
+  return number * square(number);
+}
+
 int main(void) {
   int number;
   printf("Enter a number: ");
   scanf("%d", &number);
   printf("%d is %s\n", number, is_even(number) ? "even" : "not even");
   printf("%d is %s\n", number, is_odd(number) ? "odd": "not odd");
+  printf("Square of %d is %d\n", number, square(number));
+  printf("Cube of %d is %d\n", number, cube(number));
 }
