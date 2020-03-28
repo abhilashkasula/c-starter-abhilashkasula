c0ac7c007917fe1818414cc84d43832c37db734d
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-28 20:24:10 +0530

    added print_multiplication_table function to loops.c

--- src/loops.c
+++ src/loops.c
@@ -5,13 +5,14 @@ int fibonacci(int);
 unsigned char is_even(int);
 int print_odds(int);
 int print_evens(int);
+int print_multiplication_table(int, int);
 
 unsigned char is_even(int number) {
   return number % 2 == 0 ;
 }
 
 int print_evens(int length) {
-  printf("Even numbers between 1 and %d are:\n", length);
+  printf("\nEven numbers between 1 and %d are:\n", length);
   for(int num = 1; num <= length; num++) {
     is_even(num) && printf("%d\n", num);
   }
@@ -19,7 +20,7 @@ int print_evens(int length) {
 }
 
 int print_odds(int length) {
-  printf("Odd numbers between 1 and %d are:\n", length);
+  printf("\nOdd numbers between 1 and %d are:\n", length);
   for(int num = 1; num <= length; num++) {
     !is_even(num) && printf("%d\n", num);
   }
@@ -28,7 +29,7 @@ int print_odds(int length) {
 
 int fibonacci(int length) {
   int first = 0, second = 1;
-  printf("Fibonacci Series:\n");
+  printf("\nFibonacci Series:\n");
   for(int index = 1; index <= length; index++) {
     printf("%d\n", first);
     int temp = second;
@@ -46,11 +47,20 @@ unsigned int factorial(int number) {
   return fact;
 }
 
+int print_multiplication_table(int m, int n) {
+  printf("\nMultiplication table of %d till %d:\n", m, n);
+  for(int i = 1; i <= n; i++) {
+    printf("%d\n", m*i);
+  }
+  return 0;
+}
+
 int main(void) {
   int number = 5;
-  printf("Factorial of %d is %u\n", number, factorial(number));
+  printf("\nFactorial of %d is %u\n", number, factorial(number));
   fibonacci(10);
   print_odds(10);
   print_evens(10);
+  print_multiplication_table(5, 10);
   return 0;
 }
