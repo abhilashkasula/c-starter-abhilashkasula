331dd5de892d9ec4baa72b1ec70342fc0a897748
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-28 16:11:42 +0530

    added average function to basic_functions.c

--- basic_functions.c
+++ basic_functions.c
@@ -16,10 +16,16 @@ int cube(int number) {
   return number * square(number);
 }
 
+float average(int num1, int num2, int num3) {
+  int sum = num1 + num2 + num3;
+  return sum / 3.00;
+}
+
 int main(void) {
-  int number = 5;
+  int number = 5, num2 = 7, num3 = 7;
   printf("%d is %s\n", number, is_even(number) ? "even" : "not even");
   printf("%d is %s\n", number, is_odd(number) ? "odd": "not odd");
   printf("Square of %d is %d\n", number, square(number));
   printf("Cube of %d is %d\n", number, cube(number));
+  printf("Average of %d, %d and %d is %f", number, num2, num3, average(number, num2, num3));
 }
