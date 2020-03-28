51dcce077c6ba764ece27c8992f98aa1059f0137
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-28 17:17:44 +0530

    added factorial function to loops.c

---
+++ loops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+unsigned int factorial(int number) {
+  unsigned long int fact = 1;
+  for(int index = 1; index <= number; index++) {
+    fact *= index;
+  }
+  return fact;
+}
+
+int main(void) {
+  int number = 5;
+  printf("Factorial of %d is %u\n", number, factorial(number));
+}
