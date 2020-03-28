e02a91b633a21634c973e9dc14aef32e784f3463
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-28 20:15:22 +0530

    added print_evens and print_odds functions to loops.c

--- src/loops.c
+++ src/loops.c
@@ -2,12 +2,35 @@
 
 unsigned int factorial(int);
 int fibonacci(int);
+unsigned char is_even(int);
+int print_odds(int);
+int print_evens(int);
 
-int fibonacci(int size) {
+unsigned char is_even(int number) {
+  return number % 2 == 0 ;
+}
+
+int print_evens(int length) {
+  printf("Even numbers between 1 and %d are:\n", length);
+  for(int num = 1; num <= length; num++) {
+    is_even(num) && printf("%d\n", num);
+  }
+  return 0;
+}
+
+int print_odds(int length) {
+  printf("Odd numbers between 1 and %d are:\n", length);
+  for(int num = 1; num <= length; num++) {
+    !is_even(num) && printf("%d\n", num);
+  }
+  return 0;
+}
+
+int fibonacci(int length) {
   int first = 0, second = 1;
-  printf("Fibonacci Series:  ");
-  for(int index = 1; index <= size; index++) {
-    printf("%d, ", first);
+  printf("Fibonacci Series:\n");
+  for(int index = 1; index <= length; index++) {
+    printf("%d\n", first);
     int temp = second;
     second += first;
     first = temp;
@@ -27,5 +50,7 @@ int main(void) {
   int number = 5;
   printf("Factorial of %d is %u\n", number, factorial(number));
   fibonacci(10);
+  print_odds(10);
+  print_evens(10);
   return 0;
 }
