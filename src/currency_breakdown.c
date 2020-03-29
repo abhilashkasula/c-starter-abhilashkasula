e21dfb6c28b8d0ee345c378dabf41a7d6f29600c
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-29 12:28:51 +0530

    added currency_breakdown.c

---
+++ src/currency_breakdown.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int print_denominations(unsigned long int);
+unsigned int print_note_count(unsigned long int, unsigned int);
+
+unsigned int print_note_count(unsigned long int currency, unsigned int rupee) {
+  unsigned int count = currency / rupee;
+  count > 0 && printf("%u x RS %u\n", count, rupee);
+  return currency % rupee;
+}
+
+int print_denominations(unsigned long int currency) {
+  unsigned long int remaining_balance;
+  remaining_balance = print_note_count(currency, 2000);
+  remaining_balance = print_note_count(remaining_balance, 500);
+  remaining_balance = print_note_count(remaining_balance, 200);
+  remaining_balance = print_note_count(remaining_balance, 100);
+  remaining_balance = print_note_count(remaining_balance, 50);
+  remaining_balance = print_note_count(remaining_balance, 10);
+  remaining_balance = print_note_count(remaining_balance, 5);
+  print_note_count(remaining_balance, 1);
+  return 0;
+}
+
+int main(void) {
+  unsigned long int currency = 3888;
+  printf("Amount: %lu\n\n", currency);
+  print_denominations(currency);
+  return 0;
+}
